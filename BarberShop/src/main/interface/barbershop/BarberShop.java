619f22b7124db5284990de6ed3d952f4626b259c
cleyton123 <143197269+cleyton123@users.noreply.github.com>
2023-12-14 15:03:54 -0300

    Add files via upload

---
+++ BarberShop/src/main/interface/barbershop/BarberShop.java
@@ -0,0 +1,16 @@
+package com.mycompany.barbershop;
+
+import View.Login;
+import javax.swing.JTextField;
+import javax.swing.text.View;
+
+public class BarberShop {
+    public static void main(String args[]) {
+    // Cria e exibe a tela de login diretamente
+    new Login().setVisible(true);
+}
+
+    
+
+    }
+
